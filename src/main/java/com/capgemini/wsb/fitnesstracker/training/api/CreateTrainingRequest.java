aa9c150ba1c4e18afd055696016f520f8d13e27a
sewkra <skrajczok@gmail.com>
2024-11-19 21:04:20 +0100

    added 2 endpoints

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/api/CreateTrainingRequest.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/api/CreateTrainingRequest.java
@@ -2,9 +2,9 @@ package com.capgemini.wsb.fitnesstracker.training.api;
 
 import com.capgemini.wsb.fitnesstracker.training.internal.ActivityType;
 import com.capgemini.wsb.fitnesstracker.user.api.User;
-import lombok.Data;
+import lombok.Data; //import 
 
-import java.util.Date;
+import java.util.Date; //import
 
 @Data
 public class CreateTrainingRequest {
