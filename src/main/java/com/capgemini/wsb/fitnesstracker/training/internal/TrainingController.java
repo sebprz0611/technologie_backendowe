985f644069cdb5f65ea39b64dded513034ad52b6
sebprz0611 <sprzybylek0611@gmail.com>
2024-11-20 15:24:21 +0100

    LAB 3 ( brakuje ostatniego podpunktu )
    Dodawanie nowego treningu działa ale nie przechodzi testu (?)

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -3,7 +3,6 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingService;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
